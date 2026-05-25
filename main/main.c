447d39542e2bedebfd1cfcc8b5258e01ea9608a3
Your Name <you@example.com>
2026-05-25 23:51:44 +0700

    feat: only LQR control

--- main/main.c
+++ main/main.c
@@ -5,25 +5,53 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "esp_attr.h"
+#include "stdio.h"
+#include "math.h"
 
+
+#include "encoder.h"
+#include "encoder_defs.h"
 #include "nema_motor.h"
 #include "nema_motor_defs.h"
 
 // --- Cấu hình Hardware ---
 #define STEP_GPIO       23
-#define DIR_GPIO        22
+#define DIR_GPIO_NUM        22
 #define PCNT_STEP_INPUT 21 // Nối dây từ 23 sang 21
 #define PCNT_DIR_INPUT  19 // Nối dây từ 22 sang 19
 
-// --- Thông số hành trình ---
+
 #define STEPS_PER_MM    40
 #define RANGE_MM        200
 #define MAX_STEPS       (RANGE_MM * STEPS_PER_MM) // 9000
+// #define M_PI 3.14159f
+#define DT 0.005f
+#define MAX_ACCEL 5 // m/s^2
+#define MAX_VELOCITY 5 // m/s
+#define PULLEY_CIRCUMFERENCE 0.08f // Pulley: C = 2 * pi *  R = 0.08m
+#define MICROSTEPS 3200.0f
 
-static const char* TAG = "PENDULUM_SAFETY";
+static const char* TAG = "Inverted Pendulum";
 
+encoder_t* encoder_hn3806 = NULL;
 pcnt_unit_handle_t pcnt_unit = NULL;
 motor_handle_t stepper_motor = NULL;
+pcnt_unit_handle_t encoder_hn3806_unit = NULL;
+motor_handle_t motor = NULL;
+motor_mcpwm_config_t mcpwm_config = {
+    .group_id = 0,
+    .resolution_hz = MCPWM_RESOLUTION_HZ,
+};
+motor_config_t motor_conf = {
+    .dir_gpio_num = 22,
+    .pwma_gpio_num = 23,
+    .pwm_freq_hz = 1000,
+};
+float prev_x = 0.0f;
+float prev_theta = 0.0f;
+float current_velocity = 0.0f;  
+
+const float K[4] = {-43.8476f, -130.0516f, -33.7487f, -14.3827f};
 
 static bool IRAM_ATTR on_reach_boundary(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
 {
@@ -31,12 +59,19 @@ static bool IRAM_ATTR on_reach_boundary(pcnt_unit_handle_t unit, const pcnt_watc
     return false;
 }
 
+float wrap_angle(float angle) 
+{
+    float wrapped = fmodf(angle + (float)M_PI, 2.0f * (float)M_PI);
+    if (wrapped < 0.0f) wrapped += 2.0f * (float)M_PI;
+    return wrapped - (float)M_PI;
+}
+
 void init_system() 
 {
     motor_mcpwm_config_t mcpwm_config = { .group_id = 0, .resolution_hz = 1000000 };
     motor_config_t stepper_config = {
-        .dir_gpio_num = DIR_GPIO,
-        .pwma_gpio_num = STEP_GPIO,
+        .dir_gpio_num = DIR_GPIO_NUM,
+        .pwma_gpio_num = STEP_GPIO_NUM,
         .pwm_freq_hz = 1000,
     };
     ESP_ERROR_CHECK(motor_new_mcpwm_device(&stepper_config, &mcpwm_config, &stepper_motor));
@@ -80,34 +115,133 @@ void safe_move(int speed_hz, int direction)
         return;
     }
 
-    gpio_set_level(DIR_GPIO, direction);
+    gpio_set_level(DIR_GPIO_NUM, direction);
     motor_set_speed(stepper_motor, speed_hz);
+}
+
+void set_stepper_velocity(float velocity_m_s)
+{
+    if (velocity_m_s > 0) 
+    {
+        gpio_set_level(DIR_GPIO_NUM, 1); 
+    } 
+    else if (velocity_m_s < 0) 
+    {
+        gpio_set_level(DIR_GPIO_NUM, 0); 
+    }
+
+    float abs_v = fabsf(velocity_m_s);
+    float freq_hz = (abs_v / PULLEY_CIRCUMFERENCE) * MICROSTEPS;
+
+    // Giới hạn tần số tối đa an toàn cho MCPWM (thử ở mức 20kHz - 25kHz)
+    if (freq_hz > 20000) freq_hz = 20000; 
+    
+    // Tránh tần số quá thấp gây lỗi chia cho 0 hoặc period quá lớn
+    if (freq_hz < 10) freq_hz = 0; 
+
+
+    motor_set_speed(stepper_motor, freq_hz);
+
+}
+
+void lqr_control(void *pvParameters)
+{
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    uint32_t delay_ms = (uint32_t)(DT*1000.0f);
+    const TickType_t xFrequency = pdMS_TO_TICKS(delay_ms) > 0 ? pdMS_TO_TICKS(delay_ms) : 1;
+        
+
+    while(1)
+    {
+
+        int x_steps = 0;
+        pcnt_unit_get_count(pcnt_unit, &x_steps);
+        float x = (float)x_steps * (PULLEY_CIRCUMFERENCE/ MICROSTEPS);
+        float raw_theta = 0.0f;
+        ESP_ERROR_CHECK(encoder_get_radian(encoder_hn3806, &raw_theta));
+
+        float theta = wrap_angle(raw_theta);
+
+        float x_dot = (x - prev_x) / DT;
+
+        static float filtered_theta_dot = 0;
+        float raw_theta_dot = wrap_angle(theta - prev_theta) / DT;
+        filtered_theta_dot = 0.2f * filtered_theta_dot + 0.3f * raw_theta_dot;
+
+        prev_x = x;
+        prev_theta = theta;
+
+        //u = - K * x
+        float u_accel =  (K[0] * x + K[1] * theta + K[2] * x_dot + K[3] * filtered_theta_dot);
+
+        if (u_accel > MAX_ACCEL) u_accel = MAX_ACCEL;
+        if (u_accel < -MAX_ACCEL) u_accel = -MAX_ACCEL;
+
+        static float filtered_u = 0.0f;
+    // Hệ số alpha (0.2 - 0.5). Alpha càng nhỏ, xe chạy càng mượt nhưng sẽ hơi trễ.
+    float alpha = 0.3f; 
+
+    filtered_u = (alpha * u_accel) + (1.0f - alpha) * filtered_u;
+
+    // Sử dụng filtered_u để tích phân vận tốc thay vì u_accel gốc
+        current_velocity += (filtered_u * DT);
+        // v = v0 + a*t
+        // current_velocity = current_velocity + (u_accel * DT);
+
+        if (current_velocity > MAX_VELOCITY) current_velocity = MAX_VELOCITY;
+        if (current_velocity < -MAX_VELOCITY) current_velocity = -MAX_VELOCITY;
+        
+        
+        
+        if(fabs(theta) < 0.25f)
+        {
+            set_stepper_velocity(current_velocity);
+        }
+        else
+        {
+            current_velocity = 0.0f;
+            set_stepper_velocity(0.0f);
+        }
+        ESP_LOGI(TAG,"Theta: %.3f rad | %.2f deg\n", theta, theta * 180.0f / 3.1415f);        
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+    }
+
 }
 
 void app_main(void) 
 {
-    init_system();
-    ESP_LOGI(TAG, "He thong san sang. Vi tri hien tai la TAM (0).");
-    int motor_hz = 10000;    
-    int target_direction = 1; 
-    while(1) 
-    {
-        int pos = 0;
-        pcnt_unit_get_count(pcnt_unit, &pos);
+    
+    // init_system();
+    // ESP_LOGI(TAG, "He thong san sang. Vi tri hien tai la TAM (0).");
+    // int motor_hz = 10000;    
+    // int target_direction = 1; 
+    // while(1) 
+    // {
+    //     int pos = 0;
+    //     pcnt_unit_get_count(pcnt_unit, &pos);
 
-        if (target_direction == 1 && pos >= MAX_STEPS) {
-            target_direction = 0; // Đổi sang trái
-            vTaskDelay(pdMS_TO_TICKS(500)); 
-        } 
-        else if (target_direction == 0 && pos <= -MAX_STEPS) {
-            target_direction = 1; 
-            vTaskDelay(pdMS_TO_TICKS(500)); 
-        }
+    //     if (target_direction == 1 && pos >= MAX_STEPS) {
+    //         target_direction = 0; // Đổi sang trái
+    //         vTaskDelay(pdMS_TO_TICKS(500)); 
+    //     } 
+    //     else if (target_direction == 0 && pos <= -MAX_STEPS) {
+    //         target_direction = 1; 
+    //         vTaskDelay(pdMS_TO_TICKS(500)); 
+    //     }
 
-        safe_move(motor_hz, target_direction);
+    //     safe_move(motor_hz, target_direction);
 
-        ESP_LOGI(TAG, "Vi tri: %d | Huong: %s", pos, target_direction ? "PHAI" : "TRAI");
+    //     ESP_LOGI(TAG, "Vi tri: %d | Huong: %s", pos, target_direction ? "PHAI" : "TRAI");
         
-        vTaskDelay(pdMS_TO_TICKS(20)); 
-    }
+    //     vTaskDelay(pdMS_TO_TICKS(20)); 
+    // }
+
+
+    init_system();
+    ESP_ERROR_CHECK(init_encoder(&encoder_hn3806_unit, &encoder_hn3806));
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    xTaskCreatePinnedToCore(lqr_control, "LQR_Task", 4095, NULL, 5, NULL, 1);
 }
